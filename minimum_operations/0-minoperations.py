4f2c87769bcfaf84d56c43360713e7088e9be518
mxnc <thibaultmaxence44@gmail.com>
2024-01-25 14:34:32 +0100

    minimum operation

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+method that calculates the fewest number of operations needed
+to result in exactly n H characters in the file. 
+"""
+
+def minOperations(n):
+    if type(n) is not int or n <= 1:
+        return 0
+    result = 0
+    x = 2
+    while n > 1:
+        while n % x == 0:
+            result += x
+            n = n / x
+        x += 1
+    return result
