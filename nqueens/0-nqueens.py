b2fd9ffad33981036b4eafab4478a4555c40e808
mxnctblt <thibaultmaxence44@gmail.com>
2024-04-02 10:32:34 +0200

    adding permissions

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -4,32 +4,15 @@
 import sys
 
 
-def solveNQueens(n):
-    def could_place(row, col):
-        for i in range(row):
-            if board[i] == col or \
-                board[i] - i == col - row or \
-                board[i] + i == col + row:
-                return False
-        return True
+def is_safe(board, row, col):
+    for i in range(row):
+        if board[i] == col or \
+            board[i] - i == col - row or \
+            board[i] + i == col + row:
+            return False
+    return True
 
-
-    def place_queens(n, row):
-        if row == n:
-            result.append(board[:])
-            return
-        for col in range(n):
-            if could_place(row, col):
-                board[row] = col
-                place_queens(n, row + 1)
-                board[row] = 0
-
-    result = []
-    board = [0] * n
-    place_queens(n, 0)
-    solution = [["." * i + "Q" + "." * (n - i - 1) for i in sol] for sol in result]
-    print(solution)
-
+def solve_nqueens(n):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -40,7 +23,7 @@ if __name__ == "__main__":
         if n < 4:
             print("N must be at least 4")
             sys.exit(1)
-        solveNQueens(n)
+        solve_nqueens(n)
     except ValueError:
-        print("N must be a number")
+        print("N cmust be a number")
         sys.exit(1)
