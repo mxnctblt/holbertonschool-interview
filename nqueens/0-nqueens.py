e1b640a2701845d5dc8fed2435ab750da9afa934
mxnctblt <thibaultmaxence44@gmail.com>
2024-04-04 15:24:21 +0200

    nqueens

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -5,25 +5,51 @@ import sys
 
 
 def is_safe(board, row, col):
-    for i in range(row):
-        if board[i] == col or \
-            board[i] - i == col - row or \
-            board[i] + i == col + row:
+    for i in range(col):
+        if board[i] == row or \
+           board[i] - i == row - col or \
+           board[i] + i == row + col:
             return False
     return True
 
-def solve_nqueens(n):
+
+def solve_n_queens_util(board, col, n, solutions):
+    if col == n:
+        solutions.append(list(board))
+        return
+
+    for row in range(n):
+        if is_safe(board, row, col):
+            board[col] = row
+            solve_n_queens_util(board, col + 1, n, solutions)
+
+
+def solve_n_queens(n):
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * n
+    solutions = []
+    solve_n_queens_util(board, 0, n, solutions)
+
+    for solution in solutions:
+        print([[i, solution[i]] for i in range(n)])
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
+
     try:
         n = int(sys.argv[1])
-        if n < 4:
-            print("N must be at least 4")
-            sys.exit(1)
-        solve_nqueens(n)
     except ValueError:
-        print("N cmust be a number")
+        print("N must be a number")
         sys.exit(1)
+
+    solve_n_queens(n)
