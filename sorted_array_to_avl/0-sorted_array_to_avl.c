1f33c3d256258fa7ce59cc46a5226a82fcd20750
mxnctblt <thibaultmaxence44@gmail.com>
2024-04-08 18:13:27 +0200

    fixing for betty

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,10 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - function that creates a binary tree node
- * @parent: a pointer to the parent node of the node to create
- * @value: is the value of the data
- * Return: a pointer to the new node or NULL on failure
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ *
+ * Return: pointer to the new node or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
@@ -22,12 +23,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 }
 
 /**
- * build_avl - Builds the tree
- * @array: The array to be converted
- * @start: First index of the array
- * @end: Last index of the array
- * @parent: The parent node
- * Return: A pointer to the root node of the created AVL tree, or NULL.
+ * build_avl - builds the tree
+ * @array: the array to be converted
+ * @start: first index of the array
+ * @end: last index of the array
+ * @parent: the parent node
+ *
+ * Return: a pointer to the root node of the created AVL tree, or NULL
  */
 avl_t *build_avl(int *array, int start, int end, avl_t *parent)
 {
@@ -45,17 +47,17 @@ avl_t *build_avl(int *array, int start, int end, avl_t *parent)
         return (NULL);
 
     new_node->left = build_avl(array, start, middle - 1, new_node);
-
     new_node->right = build_avl(array, middle + 1, end, new_node);
 
     return (new_node);
 }
 
 /**
- * sorted_array_to_avl - Function that builds an AVL tree from an array
- * @array: The array to be converted
- * @size: Size of the array
- * Return: A pointer to the root node of the created AVL tree, or NULL.
+ * sorted_array_to_avl - builds an AVL tree from an array
+ * @array: the array to be converted
+ * @size: size of the array
+ *
+ * Return: a pointer to the root node of the created AVL tree, or NULL
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
@@ -64,3 +66,4 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 
     return (build_avl(array, 0, size - 1, NULL));
 }
+
