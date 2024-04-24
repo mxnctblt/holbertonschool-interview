c615a4505cee77691a8367415ae9f2907aa19b7a
mxnc <thibaultmaxence44@gmail.com>
2024-04-24 08:56:00 +0200

    fixing url

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -3,7 +3,7 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
 request(url, (err, res, body) => {
   if (!err) {
